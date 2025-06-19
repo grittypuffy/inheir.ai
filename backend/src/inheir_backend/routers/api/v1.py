b1b129bfdf26390c13ae9898cc958afe82726031
Keerthana <grittypuffy@riseup.net>
2025-06-19 17:48:35 +0530

    [fix]: import reporting router properly

--- backend/src/inheir_backend/routers/api/v1.py
+++ backend/src/inheir_backend/routers/api/v1.py
@@ -4,11 +4,10 @@ from ..auth import router as auth_router
 from ..case import router as case_router
 from ..gis import router as gis_router
 from ..chatbot import router as chatbot_router
-from ..chatbot import router as report_router
+from ..report import router as report_router
 
 router = APIRouter()
 
-# Include all your routers here after importing
 router.include_router(auth_router, prefix="/auth")
 router.include_router(case_router, prefix="/case")
 router.include_router(gis_router, prefix="/gis")
